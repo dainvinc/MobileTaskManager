d7c0c1dfc88a8730ddd08fafe601af8f125d8283
Vishal K <Vishal K>
2017-03-24 19:06:26 -0400

    First build of the application

---
+++ TaskManager/app/src/main/java/com/example/visha/taskmanager/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.visha.taskmanager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+    }
+
+}
