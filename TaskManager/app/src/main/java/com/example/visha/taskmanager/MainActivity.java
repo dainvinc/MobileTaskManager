fe77ebe4b8be53f5405f44752bd149bb8beaec09
Vishal K <Vishal K>
2017-03-25 01:48:08 -0400

    Added a button feature to check out all processes

--- TaskManager/app/src/main/java/com/example/visha/taskmanager/MainActivity.java
+++ TaskManager/app/src/main/java/com/example/visha/taskmanager/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.visha.taskmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Button allButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+
+        allButton = (Button) findViewById(R.id.all);
+
+        allButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent allAppScreen = new Intent(MainActivity.this, AllApps.class);
+                startActivity(allAppScreen);
+            }
+        });
     }
 
 }
